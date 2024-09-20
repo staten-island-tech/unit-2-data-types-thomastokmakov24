b35b36e24870b36a7c1d7f9a5934788959f2fe28
thomastokmakov24 <thomast89@nycstudents.net>
2024-09-20 10:08:11 -0400

    8
    testing stuff

--- app2.py
+++ app2.py
@@ -3,7 +3,21 @@ y= x.split( )
 z = y[0]
 print(y)
 print(z) """
-
+""" import string
 x=input () 
 y=x.split()
-string.count ()
+string.count () """
+""" import string
+
+x = input("Enter a string: ")
+y = x.split()
+
+word_count = {word: y.count(word) for word in y}
+print(word_count) """
+day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+elif day_of_week == "Saturday":
+    print("")
+else:
+    print("incorrect")
