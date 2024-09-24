1a2e91503804ce079a4bb9544c623ca86d1a7524
thomastokmakov24 <thomast89@nycstudents.net>
2024-09-24 10:13:19 -0400

    10
    maybe?

--- app2.py
+++ app2.py
@@ -19,10 +19,26 @@ if x % 2 ==0:
     print("even")
 else:
     print("odd") """
+
+def calculate_tip(bill, money):
+money= float(int(input("How much was your check?")))
 bill= input("How was your customer service?")
 if bill== "bad":
-    print("0% tip?")
+    print("0% tip")
+    return money
 elif bill== "okay":
-    print("15% tip?")
+    print("15% tip")
+    return money*1.15
 elif bill== "good":
-    print("20% tip?")
+    print("20% tip")
+    return money*1.2
+elif bill== "great":
+    print("25% tip")
+    return money*1.25
+else:
+    print("response not understood. Try using the words bad,okay,good, or great.")
+
+""" 
+    tips = {'bad': 0, 'okay': 0.15, 'good': 0.20, 'great': 0.25}
+return money *  bill
+ """
